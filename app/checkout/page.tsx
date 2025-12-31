708350008ec47b5cdfc01a0833f6b08b3e00d7e3
amaanansarii <amaanansari790786@gmail.com>
2025-12-31 10:08:44 +0530

    changes done regarding adding products stripe api.

---
+++ app/checkout/page.tsx
@@ -0,0 +1,8 @@
+export default function ChecoutPage() {
+  return (
+    <div>
+      <h1>Checkout Page</h1>
+      <p>This is the checkout page. Please review your items and proceed to payment.</p>
+    </div>
+  );
+}
