ee5d65129f51de9419552400eeda3ccf3d7e7328
amaanansarii <amaanansari790786@gmail.com>
2026-01-01 07:53:51 +0530

    changes done in carousel.tsx and added card.tsx

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,5 +1,29 @@
-export default function Carousel() {
+import Stripe from "stripe"
+import { Card } from "./ui/card"
+import { useEffect, useState } from "react"
+
+interface Props {
+  products: Stripe.Product[];
+}
+
+export default function Carousel({ products }: Props) {
+
+  const [current, setCurrent] = useState<number>(0);
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+
+      setCurrent((prev) => (prev+1) % products.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  },[products.length])
+
+  const currentProduct = products[current];
+
+  const price = currentProduct.default_price as Stripe.Price;
   return (
-    <div>carousel</div>
+   <Card/>
   )
 }
