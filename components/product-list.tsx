0b2d8bd18484fe006d392847aabe269e5c75959c
amaanansarii <amaanansari790786@gmail.com>
2026-01-02 21:02:08 +0530

    ui-changes

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Stripe from "stripe";
-import ProductCard from "./products-card";
+import { ProductCard } from "./products-card";
 
 interface Props {
     products: Stripe.Product[];
@@ -11,13 +11,15 @@ export default function ProductList({ products }: Props) {
     return (
         <div>
             <div>
-                <input type="text" placeholder="Search products..."/>
+                <input type="text" placeholder="Search products..." />
             </div>
 
-            <ul>
-                {products.map((products, key) => {
-                    return <li key={key}><ProductCard product={products}></ProductCard></li>
-                })}
+            <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                {products.map((product, key) => (
+                    <li key={key}>
+                        <ProductCard product={product} />
+                    </li>
+                ))}
             </ul>
         </div>
     )
