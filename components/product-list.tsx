1887647f5948196f0c6998632d635a460f5460bf
amaanansarii <amaanansari790786@gmail.com>
2026-01-02 21:13:00 +0530

    updated productlist ui

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,26 +1,44 @@
-"use client"
+"use client";
 
 import Stripe from "stripe";
+import { useState } from "react";
 import { ProductCard } from "./products-card";
 
 interface Props {
     products: Stripe.Product[];
 }
-export default function ProductList({ products }: Props) {
+
+export const ProductList = ({ products }: Props) => {
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    const filteredProduct = products.filter((product) => {
+        const term = searchTerm.toLowerCase();
+        const nameMatch = product.name.toLowerCase().includes(term);
+        const descriptionMatch = product.description
+            ? product.description.toLowerCase().includes(term)
+            : false;
+
+        return nameMatch || descriptionMatch;
+    });
 
     return (
         <div>
-            <div>
-                <input type="text" placeholder="Search products..." />
+            <div className="mb-6 flex justify-center">
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search products..."
+                    className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
             </div>
-
             <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                {products.map((product, key) => (
+                {filteredProduct.map((product, key) => (
                     <li key={key}>
                         <ProductCard product={product} />
                     </li>
                 ))}
             </ul>
         </div>
-    )
-}
+    );
+};
