9714389e859ac2a0c15b43dc3d8baa91f33d37f7
amaanansarii <amaanansari790786@gmail.com>
2026-01-05 08:06:42 +0530

    success page

--- app/checkout/checkout-action.ts
+++ app/checkout/checkout-action.ts
@@ -23,7 +23,7 @@ export const CheckoutAction = async (formData: FormData): Promise<void> => {
         payment_method_types: ["card"],
         line_items,
         mode: "payment",
-        success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+        success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
         cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
     });
 
